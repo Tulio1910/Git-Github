3ecbd27013ab7e84f60d8b004a2ae05bb42c9045
Nicolas Lopes <nickslopes31@gmail.com>
2026-09-16 14:34:47 -0300

    Projeto inicial

---
+++ app.js
@@ -0,0 +1,14 @@
+
+alert ('Boas vindas ao jogo do numero secreto');
+let numeroSecreto = 6;
+console.log('O numero secreto é \n' +  numeroSecreto);
+let chute = prompt('Escolha um numero');
+console.log('O numero do chute é ' + chute);
+
+if (chute == numeroSecreto){
+
+alert('Isso ai! você acertou o numero secreto é \n' + numeroSecreto);
+} else if(chute > 5) {
+    alert ('o numero secreto é menor mané');
+} else alert ('o numero é maior zé');
+
